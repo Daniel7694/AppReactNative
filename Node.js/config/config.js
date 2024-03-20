4655ad2b4c79bbe0ce5cd0a47c602add7f76fe8f
Daniel7694 <107689067+Daniel7694@users.noreply.github.com>
2024-03-20 17:25:19 -0500

    (backend): add changes yn config.tsx

--- Node.js/config/config.js
+++ Node.js/config/config.js
@@ -3,7 +3,11 @@ const db = mysql.createConnection({
  host: 'localhost', 
  user: 'root', 
  password: 'kenichi769', 
- database: 'nodejs_base1'
+ database: 'nodejs_base1',
+ authPlugins : {
+    mysql_clear_password: () => Buffer.from('kenichi769','utf-8' ),
+    caching_sh2_password : true,
+ }
 });
 db.connect(function(err) {
  if (err) throw err; 
